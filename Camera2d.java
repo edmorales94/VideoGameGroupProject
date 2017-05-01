71a660b0cbbd170554300241185e9edceace208a
Edgar Morales <edmorales94@gmail.com>
2017-05-01 15:42:47 -0400

    Allowing more movement to the camera

--- Camera2d.java
+++ Camera2d.java
@@ -2,27 +2,58 @@ package groupProject;
 
 public class Camera2d
 {
-   static double x;
-   static double y;
-
-   public static void moveUpBy(int dy)
-   {
+   static int x;
+   static int y;
+   static int A = 0;
+   static boolean CAMmoving = false;
+   
+   public static void moveBy(int dx, int dy){
+		x += dx; 
+		y += dy;	
+	}
+   
+   public static void moveUpBy(int dy){
       y -= dy;
+      CAMmoving = true;
    }
 
-   public static void moveDownBy(int dy)
-   {
+   public static void moveDownBy(int dy){
       y += dy;
+      CAMmoving = true;
    }
 
-   public static void moveLeftBy(int dx)
-   {
+   public static void moveLeftBy(int dx){
       x -= dx;
+      CAMmoving = true;
    }
 
-   public static void moveRightBy(int dx)
-   {
+   public static void moveRightBy(int dx){
       x += dx;
+      CAMmoving = true;
    }
+   
+   public static void moveForwardBy(int dist){
+	   x += dist * Lookup.cos[A];
+	   y += dist * Lookup.sin[A];
+	   CAMmoving = true;
+   }
+   
+   public static void moveBackwardBy(int dist){
+	   x -= dist * Lookup.cos[A];
+	   y -= dist * Lookup.sin[A];
+	   CAMmoving = true;
+   }
+   
+   public static void rotateLeftby(int degrees){
+		A -= degrees;
+		if(A < 0) A += 360;
+	}
+   
+	public static void rotateRightby(int degrees){
+		A += degrees;
+		if(A > 359) A -= 360;
+	}
+  
+
 
 }
