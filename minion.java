06dfe0693e85e1d523d751586de6440a68368fc5
Edgar Morales <edmorales94@gmail.com>
2017-05-01 15:54:27 -0400

    This class extends Player.java
    The inheritance allows us to create a clone of the player

--- minion.java
+++ minion.java
@@ -1,24 +1,30 @@
 package groupProject;
 
-public class minion extends player{
-	public minion(double x, double y, int angle){
-		super(x,y,angle);
+public class minion extends player 
+{
+	public minion(double x, double y,int w, int h, int angle)
+	{
+		super((int)x,(int)y,w,h,angle);
 	}
-	
-	public void track(PolygonModel2d p){
-	       if(leftOrRight((int)p.x, (int)p.y) > 0)   rotateLeftby(2);
-	       if(leftOrRight((int)p.x, (int)p.y) < 0)   rotateRightby(2);
+	public void track(PolygonModel2d p)
+	   {
+	       if(leftOrRight((int)p.x, (int)p.y) > 0)  { rotateLeftby(2); this.rotateLeftrect(2);}
+	       if(leftOrRight((int)p.x, (int)p.y) < 0)  { rotateRightby(2); this.rotateRightrect(2);}
 	   }
 
-	   public void chase(PolygonModel2d p){
+
+	   public void chase(PolygonModel2d p)
+	   {
 	       track(p);
-	       if(distanceTo((int)p.x, (int)p.y) > 90)   moveForwardby(2);
+
+	       if(distanceTo((int)p.x, (int)p.y) > 100)   {moveForwardby(2); this.moveForwardrect(2);}
 	   }
-	public double leftOrRight(int x, int y){//left or right of the enemy
+	public double leftOrRight(int x, int y)//left or right of the enemy
+	{
 		return (x- this.x) * Lookup.sin[A] - (y- this.y) * Lookup.cos[A];
 	}
-	
-	public double distanceTo(int x, int y){
+	public double distanceTo(int x, int y)
+	{
 		return (x- this.x) * Lookup.cos[A] + (y- this.y) * Lookup.sin[A];
 	}
 	
